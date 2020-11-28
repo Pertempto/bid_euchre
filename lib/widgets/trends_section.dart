ab55967f7aeb1cddcba4081e6ffaf77b08f88ae0
Pertempto <pertemptodisce@gmail.com>
2020-11-27 22:50:59 -0500

    Updated trend graphs, made them scrollable when long enough.

--- lib/widgets/trends_section.dart
+++ lib/widgets/trends_section.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:bideuchre/data/data_store.dart';
 import 'package:bideuchre/data/game.dart';
 import 'package:bideuchre/data/stat_item.dart';
@@ -27,11 +25,14 @@ class _TrendsSectionState extends State<TrendsSection>
   ];
   String id;
   Data data;
+  List<Game> games;
   StatType displayStat = StatType.overallRating;
 
-  bool get wantKeepAlive => true;
-
   bool get isTeam => id.contains(' ');
+
+  int get numGames => games.length;
+
+  bool get wantKeepAlive => true;
 
   @override
   void initState() {
@@ -43,14 +44,12 @@ class _TrendsSectionState extends State<TrendsSection>
     super.build(context);
     id = widget.id;
     data = DataStore.currentData;
-    List<Game> games = data.statsDb.getGames(id, DataStore.displayArchivedStats);
+    games = data.statsDb.getGames(id, DataStore.displayArchivedStats);
     if (games.length < 2) {
       return Container();
     }
-    int numGames = min(games.length, 20);
     TextTheme textTheme = Theme.of(context).textTheme;
     List<Widget> children = [];
-    Color color = data.statsDb.getColor(id);
     children.add(ListTile(
       title: Text('Recent Trends', style: textTheme.headline6),
       dense: true,
@@ -68,83 +67,101 @@ class _TrendsSectionState extends State<TrendsSection>
         },
       ),
     ));
-    children.add(Container(
-      child: Column(
-        children: [
-          Container(
-            height: 200,
-            width: double.infinity,
-            padding: EdgeInsets.fromLTRB(24, 20, 24, 16),
-            child: LineChart(
-              LineChartData(
-                lineBarsData: List.generate(1, (index) {
-                  StatType stat = displayStat;
-                  return LineChartBarData(
-                    show: stat == displayStat,
-                    spots: List.generate(numGames, (index) {
-                      double rating;
-                      // TODO: combine different ___RatingAfterGame methods
-                      if (stat == StatType.overallRating) {
-                        rating = data.statsDb.getRatingAfterGame(id, games[index].gameId,
-                            includeArchived: DataStore.displayArchivedStats);
-                      } else if (stat == StatType.bidderRating) {
-                        rating = data.statsDb.getBidderRatingAfterGame(id, games[index].gameId,
-                            includeArchived: DataStore.displayArchivedStats);
-                      } else if (stat == StatType.winnerRating) {
-                        rating = data.statsDb.getWinnerRatingAfterGame(id, games[index].gameId,
-                            includeArchived: DataStore.displayArchivedStats);
-                      } else if (stat == StatType.setterRating) {
-                        rating = data.statsDb.getSetterRatingAfterGame(id, games[index].gameId,
-                            includeArchived: DataStore.displayArchivedStats);
-                      }
-                      rating = (rating * 10).round() / 10.0;
-                      return FlSpot((numGames - index).toDouble(), rating);
-                    }),
-                    colors: [color],
-                    barWidth: 4,
-                    isCurved: true,
-                    isStrokeCapRound: true,
-                    dotData: FlDotData(
-                      show: true,
-                      getDotPainter: (spot, percent, barData, index) =>
-                          FlDotCirclePainter(radius: 4, color: color, strokeColor: color),
-                    ),
-                  );
-                }),
-                lineTouchData: LineTouchData(
-                    touchTooltipData: LineTouchTooltipData(
-                      tooltipBgColor: Colors.grey[300].withOpacity(0.95),
-                      tooltipBottomMargin: 24,
-                      tooltipPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                      fitInsideHorizontally: true,
-                    ),
-                    touchSpotThreshold: 30,
-                    getTouchedSpotIndicator: (LineChartBarData barData, List<int> spotIndexes) {
-                      return spotIndexes.map((e) {
-                        return TouchedSpotIndicatorData(
-                            FlLine(color: Colors.grey, strokeWidth: 4, dashArray: [8]), FlDotData(show: true));
-                      }).toList();
-                    }),
-                gridData: FlGridData(show: false),
-                borderData: FlBorderData(show: false),
-                titlesData: FlTitlesData(
-                    bottomTitles: SideTitles(showTitles: false),
-                    leftTitles: SideTitles(
-                        showTitles: true,
-                        margin: 8,
-                        getTitles: (value) {
-                          return value.toStringAsFixed(1);
-                        },
-                        checkToShowTitle: (minValue, maxValue, sideTitles, appliedInterval, value) {
-                          return value == minValue || value == maxValue;
-                        })),
-              ),
-            ),
-          ),
-        ],
-      ),
-    ));
+
+    Widget lineChart = getLineChart();
+    EdgeInsets padding = EdgeInsets.fromLTRB(24, 40, 24, 16);
+    double height = 250;
+    double width = numGames * 5.0;
+    if (width < MediaQuery
+        .of(context)
+        .size
+        .width) {
+      children.add(Container(
+        height: height,
+        width: double.infinity,
+        padding: padding,
+        child: lineChart,
+      ));
+    } else {
+      children.add(SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Container(
+          height: height,
+          width: width,
+          padding: padding,
+          child: lineChart,
+        ),
+      ));
+    }
     children.add(Divider());
     return Column(crossAxisAlignment: CrossAxisAlignment.start, children: children);
+  }
+
+  Widget getLineChart() {
+    Color color = data.statsDb.getColor(id);
+    List<LineChartBarData> lineBarsData = List.generate(1, (index) {
+      StatType stat = displayStat;
+      return LineChartBarData(
+        spots: List.generate(numGames, (index) {
+          double rating;
+          // TODO: combine different ___RatingAfterGame methods
+          if (stat == StatType.overallRating) {
+            rating = data.statsDb
+                .getRatingAfterGame(id, games[index].gameId, includeArchived: DataStore.displayArchivedStats);
+          } else if (stat == StatType.bidderRating) {
+            rating = data.statsDb
+                .getBidderRatingAfterGame(id, games[index].gameId, includeArchived: DataStore.displayArchivedStats);
+          } else if (stat == StatType.winnerRating) {
+            rating = data.statsDb
+                .getWinnerRatingAfterGame(id, games[index].gameId, includeArchived: DataStore.displayArchivedStats);
+          } else if (stat == StatType.setterRating) {
+            rating = data.statsDb
+                .getSetterRatingAfterGame(id, games[index].gameId, includeArchived: DataStore.displayArchivedStats);
+          }
+          rating = (rating * 10).round() / 10.0;
+          return FlSpot((numGames - index).toDouble(), rating);
+        }),
+        colors: [color],
+        barWidth: 4,
+        isCurved: true,
+        curveSmoothness: 0.3,
+        isStrokeCapRound: true,
+        dotData: FlDotData(
+          show: false,
+        ),
+      );
+    }).toList();
+    return LineChart(
+      LineChartData(
+        lineBarsData: lineBarsData,
+        lineTouchData: LineTouchData(
+            touchTooltipData: LineTouchTooltipData(
+              tooltipBgColor: Colors.grey[300].withOpacity(0.95),
+              tooltipBottomMargin: 8,
+              tooltipPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+              fitInsideHorizontally: true,
+            ),
+            touchSpotThreshold: 30,
+            getTouchedSpotIndicator: (LineChartBarData barData, List<int> spotIndexes) {
+              return spotIndexes.map((e) {
+                return TouchedSpotIndicatorData(
+                    FlLine(color: Colors.grey, strokeWidth: 4, dashArray: [8]), FlDotData(show: true));
+              }).toList();
+            }),
+        gridData: FlGridData(show: false),
+        borderData: FlBorderData(show: false),
+        titlesData: FlTitlesData(
+            bottomTitles: SideTitles(showTitles: false),
+            leftTitles: SideTitles(
+                showTitles: true,
+                margin: 8,
+                getTitles: (value) {
+                  return value.toStringAsFixed(1);
+                },
+                checkToShowTitle: (minValue, maxValue, sideTitles, appliedInterval, value) {
+                  return value == minValue || value == maxValue;
+                })),
+      ),
+    );
   }
 }
